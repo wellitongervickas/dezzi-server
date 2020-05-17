17b59daccbcee8384f7de04a2e0d0897fbc63549
welliton gervickas <welliton@meucredere.com.br>
2020-05-16 22:44:23 -0300

    heroku deploy

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const app = require('./app');
 
 async function startServer() {
-  app.listen(process.env.SERVER_PORT, process.env.SERVER_HOST, (err) => {
+  app.listen(process.env.SERVER_PORT || 3000, process.env.SERVER_HOST || 'localhost', (err) => {
     if (err) {
       throw(err);
     }
