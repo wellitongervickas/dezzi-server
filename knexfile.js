58bf357795f2ab73db40185e7c6405eb4cf77f35
welliton gervickas <welliton@meucredere.com.br>
2020-05-16 23:42:09 -0300

    database host

--- knexfile.js
+++ knexfile.js
@@ -23,7 +23,7 @@ module.exports = {
   production: {
     client: 'pg',
     connection: {
-      host : process.env.DATABASE_URL,
+      host : process.env.DATABASE_HOST,
       user : process.env.DATABASE_USER,
       password : process.env.DATABASE_PASSWORD,
       database : process.env.DATABASE_NAME,
